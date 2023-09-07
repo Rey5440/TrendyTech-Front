4772548f654ee594c3ba8af4337df0dcc7d56705
F.Fernandez <hellsingsd@gmail.com>
2023-09-07 16:59:57 -0300

    home, paginate, card, cards

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,15 +1,45 @@
-import Cards from "../cards/cards";
-import Nav from "../nav/nav";
+import { useState, useEffect } from 'react';
+import "./home.css";
+import { useSelector } from "react-redux";
+import Cards from '../cards/cards';
+import Paginate from '../paginate/Paginate';
 
+const Home = () => {
+  const allProducts = useSelector((state) => state.allProducts);
 
-const Home = ()=>{
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 12;
 
-    return (
-        <div>
-            <Nav/>
-            <Cards/>
-        </div>
-    )
-}
+  useEffect(() => {
+    setCurrentPage(1)
+  }, [allProducts]);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    /* window.scrollTo(0, 400); */ // Scroll hacia arriba
+  };
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProduct = allProducts.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  const totalPages = Math.ceil(allProducts.length / productsPerPage);
+
+  if (allProducts.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+  <div>
+    <div>
+      <Paginate currentPage={currentPage} totalPages={totalPages} handlePageChange={handlePageChange}/>
+      <Cards currentProduct={currentProduct} />
+    </div>
+    </div>
+  );
+};
 
 export default Home;
