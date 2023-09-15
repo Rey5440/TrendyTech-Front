6dff05f29c8ee27dc4d1bc8a34989e835e6bdfbf
tomascallenius <tomascallenius96@gmail.com>
2023-09-15 10:41:24 -0300

    detail.jsx-detail.css-presentation.jsx-presentation.css

--- src/views/detail/detail.jsx
+++ src/views/detail/detail.jsx
@@ -1,12 +1,15 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Button } from "@mui/material";
 import Nav from "../../components/nav/nav";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
 import "./detail.css";
 
 const Detail = () => {
   const { id } = useParams();
   const [product, setProduct] = useState({});
+  const [imagePP, setImagePP] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,17 +25,69 @@ const Detail = () => {
     };
     fetchData();
   }, [id]);
+
+  useEffect(() => {}, [imagePP]);
+  // Cambiar la imagen principal cuando se haga clic en un botón de imagen
+  const carousel = (event) => {
+    setImagePP(product.images[event.target.value]);
+  };
   console.log(product);
   return (
     <div>
       <Nav />
-      <h1>{product.name}</h1>
-      <h3>{product.description}</h3>
-      {product.images && (
-        <img src={product.images[0]} alt={product.name} width={"300px"} />
-      )}
-      <h2>$ {product.price}</h2>
-      <button>añadir</button>
+      <div className="div_container_detail">
+        <div className="div_izquierda_detail">
+          <div className="subdiv_img">
+            {product.images && (
+              <img
+                className="product_image_focus"
+                src={imagePP || product.images[0]}
+                alt={product.name}
+              />
+            )}
+          </div>
+          <div className="divImage">
+            {product.images &&
+              product.images.map((imag, index) => (
+                <button
+                  style={{
+                    backgroundImage: `url(${imag})`,
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    width: "200px",
+                    height: "200px",
+                    margin: "6px",
+                    border: "none",
+                    borderRadius: "5px",
+                  }}
+                  // className="img_button_carousel"
+                  key={index}
+                  value={index}
+                  onClick={carousel}
+                />
+              ))}
+          </div>
+        </div>
+        <div className="div_derecha_detail">
+          <h1 className="h1_detail">{product.name}</h1>
+          <hr className="hr_detail" />
+          <h3 className="h3_detail">{product.description}</h3>
+          <h2 className="h2_stock_detail">stock: {product.stock} u.</h2>
+          <div className="div_price_button">
+          <h2 className="h2_price_detail">$ {product.price}.-</h2>
+
+          <Button
+            variant="contained"
+            className="button_agregar"
+            endIcon={<LocalMallIcon />}
+          >
+            Agregar
+          </Button>
+          </div>
+          <hr className="hr2_detail" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/views/presentation/presentation.jsx
+++ src/views/presentation/presentation.jsx
@@ -1,48 +1,164 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Button } from "@mui/material";
 import Nav from "../../components/nav/nav";
+// import Card from "../../components/card/card";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import banner1 from "../../assets/banner-publicitario-1.png";
 import banner2 from "../../assets/banner-publicitario-2.png";
 import banner3 from "../../assets/banner-publicitario-3.png";
 
+import "./presentation.css";
+
+const images = [banner1, banner2, banner3];
+const images2 = [
+  banner1,
+  banner2,
+  banner3,
+  banner1,
+  banner2,
+  banner1,
+  banner2,
+  banner3,
+  banner1,
+  banner2,
+];
+
 const Presentation = () => {
-  //hacer peticion a /users/:id para traer al admin que deberia tener una prop con las imagenes publicitarias//
-  //importar componente <Card/> para el carrousel de last products//
-  //peticion del timestamps para sacar los ultimos productos de hasta 15 dias de antiguedad//
-  
-  const [banners, setBanners] = useState(banner1);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+  const [loaded, setLoaded] = useState(false);
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      if (banners === banner1) {
-        setBanners(banner2);
-      }
-      if (banners === banner2) {
-        setBanners(banner3);
-      }
-      if (banners === banner3) {
-        setBanners(banner1);
-      }
+      selectNewImage(selectedIndex, images);
     }, 2500);
     return () => {
       clearTimeout(timer);
     };
-  }, [banners]);
+  }, [selectedImage]);
 
+  const selectNewImage = (index, images, next = true) => {
+    setLoaded(false);
+    setTimeout(() => {
+      const condition = next
+        ? selectedIndex < images.length - 1
+        : selectedIndex > 0;
+      const nextIndex = next
+        ? condition
+          ? selectedIndex + 1
+          : 0
+        : condition
+        ? selectedIndex - 1
+        : images.length - 1;
+      setSelectedImage(images[nextIndex]);
+      setSelectedIndex(nextIndex);
+    }, 200);
+  };
+
+  const previous = () => {
+    selectNewImage(selectedIndex, images, false);
+  };
+
+  const next = () => {
+    selectNewImage(selectedIndex, images);
+  };
   return (
-    <div>
+    <>
       <Nav />
-      <NavLink to="/home">
-        <button>Ingresar</button>
-      </NavLink>
-      <div>
-        <img src={banners} alt="banner" />
+      <div className="div_container_presentation">
+        <div className="button_presentation">
+          <NavLink to="/home">
+            <Button
+              variant="contained"
+              color="secondary"
+              style={{ borderRadius: "50px" }}
+              // className="button_ingresar"
+              endIcon={<RocketLaunchIcon />}
+            >
+              Ingresar
+            </Button>
+          </NavLink>
+        </div>
+        <div className="div_image_carrusel">
+          <img
+            src={selectedImage}
+            alt="publicidad"
+            className={`carousel_img ${loaded ? "loaded" : ""}`}
+            onLoad={() => setLoaded(true)}
+          />
+          <div>
+            <div className="carousel_button_prev">
+              <Button
+                startIcon={<ArrowBackIosNewIcon />}
+                onClick={previous}
+              ></Button>
+            </div>
+            <div className="carousel_button_next">
+              <Button endIcon={<ArrowForwardIosIcon />} onClick={next}></Button>
+            </div>
+          </div>
+        </div>
+        <div className="div_container_carrousel2">
+          <h2>Productos de novedad</h2>
+          <hr className="hr_presentation" />
+          <div className="div_carrousel_latest">
+            <Slider {...settings}>
+              {images2.map((image, index) => (
+                <div key={index}>
+                  <img
+                    src={image}
+                    alt={`Imagen ${index}`}
+                    className="carousel-item"
+                  />
+                </div>
+              ))}
+            </Slider>
+          </div>
+          <hr className="hr_presentation" />
+        </div>
       </div>
-      <div>
-        <h2>Productos destacados</h2>
-        carrousel
-      </div>
-    </div>
+    </>
   );
 };
 
