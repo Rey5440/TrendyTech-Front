261e4df61f3acf708d845362564f5babe74979b3
Argiiasd <mateo.p.giacosa@gmail.com>
2023-09-21 16:30:29 -0300

    commit

--- src/redux/action-types.js
+++ src/redux/action-types.js
@@ -3,3 +3,5 @@ export const SEARCH_BY_NAME = 'SEARCH_BY_NAME';
 export const ORDER_BY_PRICE = 'ORDER_BY_PRICE';
 export const ORDER_BY_NAME = 'ORDER_BY_NAME';
 export const FILTER_ALL = 'FILTER_ALL';
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
