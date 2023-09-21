01fe6b061342dfb619e4e145a60587880204fe41
Lokerrr <juan53@live.com.ar>
2023-09-21 10:29:53 -0300

    Loker

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,94 +1,124 @@
-import './filter.css';
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { filterAll, getAllProducts } from '../../redux/actions';
-import Slider from 'react-slider';
-
-
+import "./filter.css";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { filterAll, getAllProducts } from "../../redux/actions";
+import Slider from "react-slider";
 
 const Filter = () => {
   const dispatch = useDispatch();
-  const allProducts1 = useSelector(state => state.allProducts1);
-  const allProducts2 = useSelector(state => state.allProducts2);
+  const allProducts1 = useSelector((state) => state.allProducts1);
+  const allProducts2 = useSelector((state) => state.allProducts2);
   const getLowestPrice = (products) => {
-    return Math.min(...products.map(product => product.price));
-  }
+    return Math.min(...products.map((product) => product.price));
+  };
   const getHighestPrice = (products) => {
-    return Math.max(...products.map(product => product.price));
-  }
-  const MIN = getLowestPrice(allProducts2)
-  const MAX = getHighestPrice(allProducts2)
+    return Math.max(...products.map((product) => product.price));
+  };
+  const MIN = getLowestPrice(allProducts2);
+  const MAX = getHighestPrice(allProducts2);
   const [values, setValues] = useState([MIN, MAX]);
 
-  const [selectedType, setSelectedType] = useState('')
-  const [selectedBrand, setSelectedBrand] = useState('')
-
-
+  const [selectedType, setSelectedType] = useState("");
+  const [selectedBrand, setSelectedBrand] = useState("");
 
   const handleType = (event) => {
-    const typeFilter = allProducts1.filter(product => product.type === event.target.value);
-    dispatch(filterAll(typeFilter))
+    const typeFilter = allProducts1.filter(
+      (product) => product.type === event.target.value
+    );
+    dispatch(filterAll(typeFilter));
     const min = getLowestPrice(typeFilter);
     const max = getHighestPrice(typeFilter);
     setValues([min, max]);
-    setSelectedType(event.target.value)
-
-  }
+    setSelectedType(event.target.value);
+  };
 
   const handleBrand = (event) => {
-    const brandFilter = allProducts1.filter(product => product.brand === event.target.value);
-    dispatch(filterAll(brandFilter))
+    const brandFilter = allProducts1.filter(
+      (product) => product.brand === event.target.value
+    );
+    dispatch(filterAll(brandFilter));
     const min = getLowestPrice(brandFilter);
     const max = getHighestPrice(brandFilter);
     setValues([min, max]);
-    setSelectedBrand(event.target.value)
-  }
+    setSelectedBrand(event.target.value);
+  };
 
   const handleClear = () => {
-    dispatch(getAllProducts())
+    dispatch(getAllProducts());
     setValues([MIN, MAX]);
-    setSelectedType('')
-    setSelectedBrand('')
-  }
+    setSelectedType("");
+    setSelectedBrand("");
+  };
 
   const handleRange = (newValues) => {
     setValues(newValues);
-    let filterPrice = allProducts2.filter(product => 
-      product.price >= newValues[0] && product.price <= newValues[1]
-    )
-    if(selectedType !== ''){
-      const filterPriceType = filterPrice.filter(product => product.type === selectedType)
-      filterPrice = filterPriceType
+    let filterPrice = allProducts2.filter(
+      (product) =>
+        product.price >= newValues[0] && product.price <= newValues[1]
+    );
+    if (selectedType !== "") {
+      const filterPriceType = filterPrice.filter(
+        (product) => product.type === selectedType
+      );
+      filterPrice = filterPriceType;
     }
-    if(selectedBrand !== ''){
-      const filterPriceBrand = filterPrice.filter(product => product.brand === selectedBrand)
-      filterPrice = filterPriceBrand
-      }
-    
+    if (selectedBrand !== "") {
+      const filterPriceBrand = filterPrice.filter(
+        (product) => product.brand === selectedBrand
+      );
+      filterPrice = filterPriceBrand;
+    }
 
-
-/* if(filterPriceType < 1) return */
-      dispatch(filterAll(filterPrice))
-  }
+    /* if(filterPriceType < 1) return */
+    dispatch(filterAll(filterPrice));
+  };
   return (
     <div className="FilterTech">
       <div>
-      <select onChange={handleType} /* value={selectedType}  */ defaultValue=''>
-    <option value=''  selected hidden >Seleccione una categoria</option>
-          {allProducts1.length > 0 && Array.from(new Set(allProducts1.map(product => product.type))).map((type, index) => {
-          return <option key={index} value={type}>{type}</option>;
-        })}
+        <select
+          onChange={handleType}
+          /* value={selectedType}  */ defaultValue=""
+        >
+          <option value="" selected hidden>
+            Seleccione una categoria
+          </option>
+          {allProducts1.length > 0 &&
+            Array.from(
+              new Set(allProducts1.map((product) => product.type))
+            ).map((type, index) => {
+              return (
+                <option key={index} value={type}>
+                  {type}
+                </option>
+              );
+            })}
         </select>
       </div>
       <div>
-      <select onChange={handleBrand} /* value='' */defaultValue=''>
-    <option  value='' selected hidden >Seleccione una marca</option>
-       {allProducts1.length > 0 && Array.from(new Set(allProducts1.map(product => product.brand))).map((brand, index) => {
-          return <option key={index} value={brand}>{brand}</option>;
-        })}
+        <select onChange={handleBrand} /* value='' */ defaultValue="">
+          <option value="" selected hidden>
+            Seleccione una marca
+          </option>
+          {allProducts1.length > 0 &&
+            Array.from(
+              new Set(allProducts1.map((product) => product.brand))
+            ).map((brand, index) => {
+              return (
+                <option key={index} value={brand}>
+                  {brand}
+                </option>
+              );
+            })}
         </select>
       </div>
-      <div style={{ padding: '20px', border: '1px solid #ddd', borderRadius: '5px', boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)' }}>
+      <div
+        style={{
+          padding: "20px",
+          border: "1px solid #ddd",
+          borderRadius: "5px",
+          boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)",
+        }}
+      >
         <h2>Price Range</h2>
         <p>Use the slider to select a price range:</p>
         <Slider
@@ -98,7 +128,7 @@ const Filter = () => {
           min={MIN}
           max={MAX}
         />
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
           <div>
             <label htmlFor="minPrice">Min Price:</label>
             <input
