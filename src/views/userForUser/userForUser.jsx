dc7e1affb0d23bab8d841afe234d7747adefd1d8
F.Fernandez <hellsingsd@gmail.com>
2023-09-21 23:49:28 -0300

    holis

--- src/views/userForUser/userForUser.jsx
+++ src/views/userForUser/userForUser.jsx
@@ -1,8 +1,25 @@
-
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useState } from "react";
 
 const UserForUser = () => {
+    const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+    console.log(user)
+    console.log(getAccessTokenSilently())
+    const [accessToken, setAccessToken] = useState(null)
+    accessToken && console.log(accessToken)
+    const obtenerToken = async () => {
+        try {
+          const token = await getAccessTokenSilently();
+          setAccessToken(token);
+        } catch (error) {
+          console.error("Error al obtener el token de acceso:", error);
+        }
+      };
 
-
+    useEffect(()=>{
+        obtenerToken()
+    },[])  
+    
     return (
         <div>
             <img src='https://definicion.de/wp-content/uploads/2019/07/perfil-de-usuario.png'></img>
@@ -14,3 +31,5 @@ const UserForUser = () => {
 }
 
 export default UserForUser;
+
+/* getAccessTokenSilently */
