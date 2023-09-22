8d6e5cb0d8b5f128397b08717f691d3770c40687
00JAS00 <julian.m.sosa.muchut@gmail.com>
2023-09-22 02:57:25 -0300

    pasarela de pago

--- src/components/shopping_cart/shopping_cart.jsx
+++ src/components/shopping_cart/shopping_cart.jsx
@@ -3,13 +3,40 @@ import { useSelector } from 'react-redux';
 import CartItem from './cart_item';
 import Nav from '../../components/nav/nav'
 import Footer from '../../views/footer/footer'
+import axios from 'axios';
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react' 
 import './shopping_cart.css';
 
+initMercadoPago('TEST-185b7434-044a-4830-995d-95780e762ec5');
 const ShoppingCart = () => {
     let cart = useSelector(state => state.shoppingCart);
     let [total, setTotal] = useState(0);
+    let [preferenceId, setPreferenceId] = useState(null);
     let totalProductsInCart = cart.reduce((acc, product) => acc + product.quantity, 0);
-
+    
+    // Inicio de compra mp
+    const handleBuy=async()=>{
+        const id=await createPreference(cart);
+        if(id) {setPreferenceId(id)}
+      }
+    const createPreference = async (cart) => {
+        let productos=cart.map((product)=>{
+            return {
+                title:product.name,
+                unit_price:Number(product.price),
+                picture_url:product.images[0],
+                quantity:Number(product.quantity),
+            }
+        })
+        try {
+            const response= await axios.post('http://localhost:3004/checkout/create_preference',{productos});
+            const id=response.data
+            return id;//id de compra solo eso
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    // ! fin de compra mp
     let handleTotal = () => {
         let total = 0;
         cart.forEach(product => {
@@ -46,11 +73,16 @@ const ShoppingCart = () => {
                                         <div>
                                             <h3>Producto</h3>
                                             <p>${total}</p>
+                                            <button onClick={handleBuy}>Confirmacion</button>
+                                            {console.log(preferenceId)}{preferenceId && <Wallet initialization={{preferenceId:preferenceId}}/>}
+                                            
                                         </div>
                                     ) : (
                                         <div>
                                             <h3>Productos ({totalProductsInCart})</h3>
                                             <p>${total}</p>
+                                            <button onClick={handleBuy}>Confirmacion</button>
+                                            {preferenceId && <Wallet initialization={{preferenceId}}/>}
                                         </div>
                                     )}
                                 </div>
