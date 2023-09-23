d4d47742e6924422f8203ca199e24101e36d1865
Lokerrr <juan53@live.com.ar>
2023-09-23 16:54:11 -0300

    changes

--- src/components/cards/emptyCards.jsx
+++ src/components/cards/emptyCards.jsx
@@ -1,4 +1,5 @@
 import { Container, Box } from "@mui/system";
+import iconoCarrito from "../../assets/online-shopping (1).png";
 export default function Empty() {
   return (
     <Container
@@ -7,10 +8,32 @@ export default function Empty() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        width: '100%',
+        width: "100%",
       }}
     >
-      <Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "1rem",
+          backgroundColor: "white",
+          width: "40%",
+          height: "25rem",
+          borderRadius: "5px",
+          boxShadow: "0px 0px 3px 0.01px rgba(0, 0, 0, 0.14)",
+        }}
+      >
+        <img
+          src={iconoCarrito}
+          alt="carrito vacio"
+          className="empty-cart-icon"
+        />
+        <h2>¡Tu carrito está vacío!</h2>
+        <NavLink to="/home" className="home-button">
+          Ver productos
+        </NavLink>
         <h4>Lo siento! No existen productos con esas características</h4>
       </Box>
     </Container>
