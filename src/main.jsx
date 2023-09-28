ff275797d9290d1f0aa8647db45d767a306def1a
Lokerrr <juan53@live.com.ar>
2023-09-28 18:46:27 -0300

    BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,26 @@
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import store from "./redux/store.js";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <Auth0Provider
-      domain="dev-6u1rw8b065enk7nb.us.auth0.com"
-      clientId="q8WshN0PeWFSr3lZlvXkBx0blXS5SwjZ"
-      authorizationParams={{
-        redirect_uri: window.location.origin,
-      }}
-    >
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Auth0Provider>
-  </Provider>
+  <StrictMode>
+    <Provider store={store}>
+      <Auth0Provider
+        domain="dev-6u1rw8b065enk7nb.us.auth0.com"
+        clientId="q8WshN0PeWFSr3lZlvXkBx0blXS5SwjZ"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Auth0Provider>
+    </Provider>
+  </StrictMode>
 );
