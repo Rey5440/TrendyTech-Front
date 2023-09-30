0421e48d1037862b38a3593f7eadde24d08abe08
tomascallenius <tomascallenius96@gmail.com>
2023-09-29 23:47:03 -0300

    ahora si tenemos cookies

--- src/helpers/autenticateAllUsers.js
+++ src/helpers/autenticateAllUsers.js
@@ -1,5 +1,12 @@
 import Cookies from "js-cookie";
 import axios from "axios";
+import generateToken from "./generateToken";
+
+const getToken = (user) => {
+  const token = generateToken();
+  user.token = token;
+  return user;
+};
 
 const saveUserDataToCookie = (user) => {
   // Genera una clave única para cada usuario (por ejemplo, el ID de usuario)
@@ -14,31 +21,40 @@ const saveUserDataToCookie = (user) => {
   }
 };
 
+const getUser = async (user) => {
+  try {
+    const response = await axios.get(`http://localhost:3004/users/token/${user}`);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const postUser = async (user) => {
   try {
     const response = await axios.post("http://localhost:3004/users/auth", user);
-
-    saveUserDataToCookie(user);
-    // console.log("pase por aca");
+    saveUserDataToCookie(user.token);
   } catch (error) {
     console.log(error);
   }
 };
 
 // Función para obtener los datos del usuario desde la cookie
-const getUserDataFromCookie = (user) => {
-  // console.log("pase por get cookie");
-
+const getUserDataFromCookie = async(user) => {
   // Genera la clave única para el usuario
   try {
     // Obtiene la cadena JSON de la cookie "userKey"
     const userDataJSON = Cookies.get("ignacioMagic");
-    
+    console.log(userDataJSON);
     // Si la cookie existe, la parsea de JSON a un objeto
     if (userDataJSON) {
-      return JSON.parse(userDataJSON);
+      const user = JSON.parse(userDataJSON);
+      const tal = await getUser(user)
+      return tal;
+      console.log(tal);
     } else if (user.email) {
-      postUser(user);
+      const userWithToken = getToken(user);
+      postUser(userWithToken);
     }
   } catch (error) {
     console.log(error);
@@ -48,10 +64,9 @@ const getUserDataFromCookie = (user) => {
 };
 
 const autenticateAllUsers = (user) => {
-  // console.log(user);
-
-  getUserDataFromCookie(user);
+ const result = getUserDataFromCookie(user);
   // Función para guardar datos del usuario en una cookie
+  return result;
 };
 
 export default autenticateAllUsers;
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,56 +1,65 @@
-import axios from 'axios';
+import axios from "axios";
 const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
+import {
+  GET_ALL_PRODUCTS,
+  SEARCH_BY_NAME,
+  ORDER_BY_NAME,
+  ORDER_BY_PRICE,
+  FILTER_ALL,
+  SHOW_ALERT,
+  HIDE_ALERT,
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  INCREASE_QUANTITY,
+  DECREASE_QUANTITY,
+  USER_DATA,
+} from "./action-types";
 
-import { GET_ALL_PRODUCTS, SEARCH_BY_NAME, ORDER_BY_NAME, ORDER_BY_PRICE, FILTER_ALL, SHOW_ALERT, HIDE_ALERT, ADD_TO_CART, REMOVE_FROM_CART, INCREASE_QUANTITY, DECREASE_QUANTITY } from './action-types';
-
-export const getAllProducts = ()=>{
-    return async function (dispatch) {
-        try{
-            const all = await axios(
-              `${VITE_BACKEND_URL}/products`
-            );
-            console.log(all)
-            return dispatch({
-                type:GET_ALL_PRODUCTS,
-                payload: all.data
-            })
-        }catch (error) {
-            console.log(error)
-        }
+export const getAllProducts = () => {
+  return async function (dispatch) {
+    try {
+      const all = await axios(`${VITE_BACKEND_URL}/products`);
+      return dispatch({
+        type: GET_ALL_PRODUCTS,
+        payload: all.data,
+      });
+    } catch (error) {
+      console.log(error);
     }
-}
+  };
+};
 
 export const searchByName = (product) => {
-    return async function (dispatch) {
-        console.log(product)
-        try{
-            const foundProduct = await axios(
-              `${VITE_BACKEND_URL}/products/?name=${product}`
-            );
-            console.log(foundProduct.data)
-            return dispatch({
-                type:SEARCH_BY_NAME,
-                payload: foundProduct.data
-            })
-        }catch (error) {
-            console.log("searchByName error: ", error.message);
-        }
+  return async function (dispatch) {
+    console.log(product);
+    try {
+      const foundProduct = await axios(
+        `${VITE_BACKEND_URL}/products/?name=${product}`
+      );
+      console.log(foundProduct.data);
+      return dispatch({
+        type: SEARCH_BY_NAME,
+        payload: foundProduct.data,
+      });
+    } catch (error) {
+      console.log("searchByName error: ", error.message);
     }
-}
+  };
+};
 
 export const orderByPrice = (payload) => {
-    return {
-        type: ORDER_BY_PRICE,
-        payload: payload
-    }
-}
+  return {
+    type: ORDER_BY_PRICE,
+    payload: payload,
+  };
+};
 
 export const orderByName = () => {
-    return {
-        type: ORDER_BY_NAME
-    }
-}
+  return {
+    type: ORDER_BY_NAME,
+  };
+};
 
 /* export const filterByColor = () => {
     return {
@@ -59,61 +68,69 @@ export const orderByName = () => {
 } */
 
 export const filterAll = (payload) => {
-    return function (dispatch) {
-            return dispatch({
-                type:FILTER_ALL,
-                payload: payload
-            })
-    }
-}
+  return function (dispatch) {
+    return dispatch({
+      type: FILTER_ALL,
+      payload: payload,
+    });
+  };
+};
 
 export const setAlert = (message, type) => {
-    return dispatch => {
-        dispatch({
-            type: SHOW_ALERT,
-            payload: {
-                message,
-                type
-            }
-        });
+  return (dispatch) => {
+    dispatch({
+      type: SHOW_ALERT,
+      payload: {
+        message,
+        type,
+      },
+    });
 
-        // Limpia el alerta después de 3 segundos
-        setTimeout(() => {
-            dispatch({
-                type: HIDE_ALERT
-            });
-        }, 3000);
-    }
-}
+    // Limpia el alerta después de 3 segundos
+    setTimeout(() => {
+      dispatch({
+        type: HIDE_ALERT,
+      });
+    }, 3000);
+  };
+};
 
 //http://localhost:3004/products/filter?color=1&type=1&brand=3&minPrice=100&maxPrice=100000
 // ejemplo de ruta pata filtro combinado
 
 export const addToCart = (product) => {
-    console.log("producto añadido al carrito: ", product)
-    return {
-        type: ADD_TO_CART,
-        payload: product
-    }
-}
+  console.log("producto añadido al carrito: ", product);
+  return {
+    type: ADD_TO_CART,
+    payload: product,
+  };
+};
 
 export const removeFromCart = (id) => {
-    return {
-        type: REMOVE_FROM_CART,
-        payload: id
-    }
-}
+  return {
+    type: REMOVE_FROM_CART,
+    payload: id,
+  };
+};
 
 export const increaseQuantity = (id) => {
-    return {
-        type: INCREASE_QUANTITY,
-        payload: id
-    }
-}
+  return {
+    type: INCREASE_QUANTITY,
+    payload: id,
+  };
+};
 
 export const decreaseQuantity = (id) => {
-    return {
-        type: DECREASE_QUANTITY,
-        payload: id
-    }
-}
+  return {
+    type: DECREASE_QUANTITY,
+    payload: id,
+  };
+};
+
+export const getuserData = (payload) => {
+  console.log(payload);
+  return {
+    type: USER_DATA,
+    payload: payload,
+  };
+};
